f6500f65bccba5fb533e2751bf0abff7dc2860cb
Himanshi Singh <singh.himanshi004@gmail.com>
2018-02-12 22:10:31 -0500

    added files

---
+++ Afdriver.java
@@ -0,0 +1,48 @@
+package org;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.*;
+
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class Afdriver {
+
+	/**
+	 * @param args
+	 * @throws IOException 
+	 * @throws IllegalArgumentException 
+	 */
+	public static void main(String[] args) throws IllegalArgumentException, IOException {
+		// TODO Auto-generated method stub
+		Configuration conf=new Configuration();
+		Job job=Job.getInstance(conf);
+		job.setJarByClass(Afdriver.class);
+		job.setMapperClass(Afmapper.class);
+		job.setReducerClass(Afreducer.class);
+		
+		//job.setNumReduceTasks(2); It will run reducer twice.
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		
+		FileInputFormat.addInputPath(job,new Path("afghaninput"));
+		FileOutputFormat.setOutputPath(job,new Path("afghanout"));
+		
+		try {
+			job.waitForCompletion(true);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+}
